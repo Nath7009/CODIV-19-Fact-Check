df9dfbed710a560cca8c460f020fb0ddcce081a4
Nath7009 <cantatnathan@gmail.com>
2020-03-18 19:37:02 +0200

    Added the connexion between frontend and backend, updated packages

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to rel2oad.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+
+  constructor(props) {
+    super(props);
+    this.state = { backResponse: "" };
+  }
+
+  callBack() {
+    fetch("http://localhost:9000/testBack")
+        .then(res => res.text())
+        .then(res => this.setState({ backResponse: res }));
+  }
+
+  componentWillMount() {
+    this.callBack();
+  }
+
+  render(){
+    return  (
+      <div  className="App">
+            <header  className="App-header">
+                <img  src={logo}  className="App-logo"  alt="logo"  />
+                <p>
+                    Edit  <code>src/App.js</code>  and  save  to  rel2oad.
+                </p>
+                <p className="App-intro">;{this.state.backResponse}</p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener  noreferrer"
+                >
+                    Learn  React
+                </a>
+            </header>
+        </div>
+    );
+  }
 }
 
 export default App;
